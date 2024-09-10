2ac410d2a08c5516b2a97583ed1e28bbbff96f3e
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-09 20:44:23 -0400

    init file

---
+++ CountCharacters.java
@@ -0,0 +1,10 @@
+package CountCharacters;
+
+import java.util.Scanner;
+
+public class CountCharacters {
+   public static void main(String[] args) {
+    Scanner scnr = new Scanner(System.in);
+      
+   }
+}
