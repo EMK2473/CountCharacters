a58021037f2fc6f82d2e3c31124e65de9bc3e574
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-09 21:47:51 -0400

    added condition for single count print

--- CountCharacters.java
+++ CountCharacters.java
@@ -9,16 +9,20 @@ public class CountCharacters {
     char character = scnr.next().charAt(0);
     String string = scnr.next();
     int count = 0;
-    
-    System.out.println("Character: " + character);
-    System.out.println("String: " + string);
+   
 
     for (int i = 0; i < string.length(); ++i) {
       if(string.charAt(i) == character){
          count++;
       }
     }
-    System.out.println(count + " " + character + "'s");
-    scnr.close();
+
+    if(count != 1) {
+      System.out.println(count + " " + character + "'s");
+    }
+    else {
+       System.out.println(count + " " + character);
    }
+   scnr.close();
+}
 }
