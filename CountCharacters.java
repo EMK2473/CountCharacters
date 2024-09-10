d5f1eec46c60d88b3d47c2258478edeff5765cac
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-09 21:40:32 -0400

    added for loop, checking if character matches index of string; incrementing count; print in format

--- CountCharacters.java
+++ CountCharacters.java
@@ -6,11 +6,19 @@ public class CountCharacters {
    public static void main(String[] args) {
     Scanner scnr = new Scanner(System.in);
     
-    String character = scnr.next();
+    char character = scnr.next().charAt(0);
     String string = scnr.next();
+    int count = 0;
     
     System.out.println("Character: " + character);
     System.out.println("String: " + string);
+
+    for (int i = 0; i < string.length(); ++i) {
+      if(string.charAt(i) == character){
+         count++;
+      }
+    }
+    System.out.println(count + " " + character + "'s");
     scnr.close();
    }
 }
