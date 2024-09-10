294e35ea433f8a3c69e58d490a91ebd3d30e5bad
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-09 20:50:50 -0400

    assigned and declared input variables; tested input variables with prints

--- CountCharacters.java
+++ CountCharacters.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class CountCharacters {
    public static void main(String[] args) {
     Scanner scnr = new Scanner(System.in);
-      
+    
+    String character = scnr.next();
+    String string = scnr.next();
+    
+    System.out.println("Character: " + character);
+    System.out.println("String: " + string);
+    scnr.close();
    }
 }
